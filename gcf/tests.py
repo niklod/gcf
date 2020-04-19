6ba4d28b3bcaf22c4b9c72a42cbd2f42f5b4f190
nick <nik.lodyanoy@yandex.ru>
2020-04-19 16:04:06 +0300

    fixed namespace in urls

--- gcf/tests.py
+++ gcf/tests.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
-
+from django.urls import reverse
 from .models import Player
 
 
 class PlayerViewsTest(TestCase):
 
     def test_player_list_view(self):
-        response = self.client.get('/players/')
+        response = self.client.get(reverse('gcf:players_list'))
 
         self.assertIs(response.status_code, 200)
 
@@ -14,7 +14,8 @@ class PlayerViewsTest(TestCase):
         test_player = Player(name='Test Player', age=123, description='Test Description')
         test_player.save()
 
-        response = self.client.get(f'/players/{test_player.pk}/')
+        # response = self.client.get(f'/players/{test_player.pk}/')
+        response = self.client.get(reverse('gcf:player_detail', kwargs={'pk': test_player.pk}))
 
         self.assertIs(response.status_code, 200)
         self.assertContains(response, test_player.name)
