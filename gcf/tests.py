1ecaa5aacf3b0d08daee7c6c03ea371d70e1a1c6
nick <nik.lodyanoy@yandex.ru>
2020-04-19 15:54:08 +0300

    tests for Player views

--- gcf/tests.py
+++ gcf/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Player
+
+
+class PlayerViewsTest(TestCase):
+
+    def test_player_list_view(self):
+        response = self.client.get('/players/')
+
+        self.assertIs(response.status_code, 200)
+
+    def test_player_detail_view(self):
+        test_player = Player(name='Test Player', age=123, description='Test Description')
+        test_player.save()
+
+        response = self.client.get(f'/players/{test_player.pk}/')
+
+        self.assertIs(response.status_code, 200)
+        self.assertContains(response, test_player.name)
