4476aa823a142869b0c29f9866d61a475b56e684
nick <nik.lodyanoy@yandex.ru>
2020-04-19 13:51:43 +0300

    added view for Player model

--- gcf/urls.py
+++ gcf/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import PlayerView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('players/', PlayerView.as_view(), name='players_list'),
 ]
--- gcf/views.py
+++ gcf/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from .models import Player
 
 
-# Create your views here.
-def index(request):
-    return HttpResponse('<h1>hello</h1>')
+class PlayerView(generic.ListView):
+    model = Player
+    context_object_name = 'players_list'
