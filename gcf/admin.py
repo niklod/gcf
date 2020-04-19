bca8a7ebbf9f9beab9b0cbacbab66e3c99f4537f
nick <nik.lodyanoy@yandex.ru>
2020-04-19 13:32:25 +0300

    Added model "Player"

--- gcf/admin.py
+++ gcf/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from gcf.models import User, Player
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Player)
--- gcf/models.py
+++ gcf/models.py
@@ -16,3 +16,22 @@ class User(models.Model):
             self.created_at = timezone.now()
         self.updated_at = timezone.now()
         return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.username
+    
+    def __repr__(self):
+        return f'<User(username: {self.username}, id: {self.id})>'
+
+
+class Player(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100, null=False)
+    age = models.IntegerField(null=True)
+    description = models.CharField(max_length=300, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f'<Player(Name: {self.name}, id: {self.id})>'
