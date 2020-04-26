6dbd4d7a309042df4c82380b75bdb556c4e12ada
nick <nik.lodyanoy@yandex.ru>
2020-04-26 14:32:18 +0300

    hide slug field

--- gcf/admin.py
+++ gcf/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from gcf.models import User, Player
 
 
+class PlayerAdmin(admin.ModelAdmin):
+    exclude = ('slug',)
+
+
+admin.site.register(Player,PlayerAdmin)
 admin.site.register(User)
-admin.site.register(Player)
