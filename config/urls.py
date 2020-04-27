fffeb3d7e23bfb3736a24cc97f99534626e9488a
nick <nik.lodyanoy@yandex.ru>
2020-04-28 00:40:36 +0300

    changer app url

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('gcf.urls')),
+    path('games/', include('gcf.urls')),
     path('admin/', admin.site.urls),
 ]
