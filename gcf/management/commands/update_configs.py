a31493779d7e41ae4db2c049ff0da11659e4210f
nick <nik.lodyanoy@yandex.ru>
2020-05-03 19:21:12 +0300

    added command to update players info

--- gcf/management/commands/update_configs.py
+++ gcf/management/commands/update_configs.py
@@ -1,11 +1,9 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from ...utils.catalog_updater import CatalogUpdater
-from ...utils.parsers.prosettings_parser import CsProSettingsParser
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         updater = CatalogUpdater()
         updater.update_configs()
-        # parser = CsProSettingsParser()
-        # print(parser.parse_all_csgo_players())
+        updater.update_info()
