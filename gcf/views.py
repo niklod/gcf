a73c1a174d4caf659a42ceeaffee959ed5c4987e
nick <nik.lodyanoy@yandex.ru>
2020-05-05 22:30:05 +0300

    added avg data for player stats

--- gcf/views.py
+++ gcf/views.py
@@ -1,7 +1,9 @@
 from django.http import Http404
 from django.views import generic
-from .models import Player, Game, PlayerConfig
+from .models import Player, Game, PlayerConfig, PlayerStats
 from django.shortcuts import get_object_or_404
+from django.db.models import Avg, F, FloatField
+from django.db.models.functions import Cast
 
 
 class PlayerView(generic.ListView):
@@ -18,8 +20,42 @@ class PlayerDetailView(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['game'] = get_object_or_404(Game, slug=self.kwargs.get('game_slug', ''))
-        config = PlayerConfig.objects.filter(game__id=context['game'].id, player__id=context['player'].id).first()
+        context['game'] = get_object_or_404(Game,
+                                            slug=self.kwargs.get(
+                                                'game_slug', ''))
+
+        kda_annotate = Cast(F('total_kills'),
+                            output_field=FloatField()) / Cast(
+                                F('total_deaths'), output_field=FloatField())
+
+        kill_per_round_annotate = Cast(
+            F('total_kills'), output_field=FloatField()) / Cast(
+                F('rounds_played'), output_field=FloatField())
+
+        death_per_round_annotate = Cast(
+            F('total_deaths'), output_field=FloatField()) / Cast(
+                F('rounds_played'), output_field=FloatField())
+
+        context['stats_avg'] = PlayerStats.objects.annotate(
+            kill_per_round=kill_per_round_annotate,
+            kda=kda_annotate,
+            death_per_round=death_per_round_annotate).all().aggregate(
+                Avg('rating'), Avg('total_kills'), Avg('total_deaths'),
+                Avg('rounds_played'), Avg('damage_per_round'),
+                Avg('grenade_damage_per_round'), Avg('assists_per_round'),
+                Avg('headshots_percent'), Avg('kill_per_round'),
+                Avg('death_per_round'), Avg('kda'))
+
+        context['player_stats'] = PlayerStats.objects.filter(
+            game__id=context['game'].id,
+            player__id=context['player'].id).annotate(
+                kda=kda_annotate,
+                kill_per_round=kill_per_round_annotate,
+                death_per_round=death_per_round_annotate).first()
+
+        config = PlayerConfig.objects.filter(
+            game__id=context['game'].id,
+            player__id=context['player'].id).first()
 
         if config:
             context['config'] = config
